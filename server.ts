feee8db1cf5da224aaa7c897eb939eb08b3ced08
BODEDDULA PRAVEEN KUMAR <bkpraveen0325@gmail.com>
2026-05-03 01:03:18 +0530

    Change server port to use environment variable

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ console.log('--- SERVER STARTING UP ---');
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
